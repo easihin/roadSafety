559bfe992badf3ca933ff0f3b393c58704199789
Ella Asihin <64117637+easihin@users.noreply.github.com>
2022-04-16 14:53:39 +0700

    Update positioning.cpp

--- positioning.cpp
+++ positioning.cpp
@@ -2,13 +2,15 @@
 
 ADXL345 accel;
 
+using namespace std;
+
 // callibration value
 float callVal = 256.00;
 
 class Position {
     public: 
         float callibrate(float val, float offset) {
-            return (val + offset)/callVal;
+            return (val + offset);
         }
         int pitch(float x, float y, float z) {
             return atan(-1 * x / sqrt(pow(y, 2) + pow(z, 2))) * 180 / PI;
@@ -44,8 +46,8 @@ void checkPositions() {
     float newY = current.callibrate(yVal, yOffset);
     float newZ = current.callibrate(zVal, zOffset);
 
-    float roll = current.roll(newX, newY, newZ);
-    float pitch = current.pitch(newX, newY, newZ);
+    float roll = current.roll(newX/callVal, newY/callVal, newZ/callVal);
+    float pitch = current.pitch(newX/callVal, newY/callVal, newZ/callVal);
 
     float rollF = current.angle(roll);
     float pitchF = current.angle(pitch);
@@ -54,14 +56,15 @@ void checkPositions() {
     int higherBound = 256 + 50;
     
     // if flipped
-    if (fabs(pitchF > 1.5) || (!(zVal + zOffset > lowerBound) && !(zVal + zOffset < higherBound))) {
-        sendMessage(1);
-        isSafe();
+    if (fabs(pitchF > 1.5) || (!(newZ > lowerBound) && !(newZ < higherBound))) {
+      int i = 1;
+      isSafe(i);
     }
 
     // extreme deceleration
-    if (yVal + yOffset < -100 || fabs(rollF) > 1.5) {
-        isSafe();
+    if (newY < -100 || fabs(rollF) > 1.5) {
+      int i = 2;
+      isSafe(i);
      }
 
     Serial.print("pitchF: ");
@@ -73,4 +76,27 @@ void checkPositions() {
 
 void initAccel() {
     accel.initialize();
+}
+
+// 10 second timer
+void isSafe(int i) {
+  int seconds = 0;
+  bool toRun = true;
+  int message = 3;
+  while (toRun == true) {
+      printLCD(message);
+      delay(1000);
+      
+      seconds++;
+      cout << seconds << " sec " << endl;
+ 
+      if (seconds >= 10) {
+        message = 4;
+        sendMessage(i);
+        Serial.println("not safe");
+     } else if (checkPushed() == 1) {
+        toRun = false;
+        Serial.println("safe");
+     }
+   }
 }
